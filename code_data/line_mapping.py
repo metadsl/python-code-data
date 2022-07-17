3be14a7ddc252eedb99a42af1b5ba83aeae4abad
Saul Shanabrook <s.shanabrook@gmail.com>
2022-07-17 19:03:29 -0400

    again

--- code_data/line_mapping.py
+++ code_data/line_mapping.py
@@ -196,9 +196,9 @@ def collapse_items(items: ExpandedItems, is_linetable: bool) -> CollapsedItems:
     # Iterate over the items, from the end to the begining.
     # If there is a zero and the previous is at the limit,
     # then remove the current, and add it to the previous.
-    print("SLOW VERSION")
+    print("SLOW VERSION22")
     x = 0
-    for i in range(100000000):
+    for i in range(1000000):
         x = x + 1
     for i in range(len(items) - 1, 0, -1):
         item = collapsed_items[i]
