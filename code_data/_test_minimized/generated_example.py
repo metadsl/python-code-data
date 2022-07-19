d8f5e8d87eb66a605d5940ba9acfb92acaf20902
Saul Shanabrook <s.shanabrook@gmail.com>
2022-07-19 14:36:42 -0400

    Fix failing test case

---
+++ code_data/_test_minimized/generated_example.py
@@ -0,0 +1,106 @@
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A00:
+    pass
+
+
+class A00:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+class A:
+    pass
+
+
+while False:
+    pass
+while 0.0_1 and 0x1 + {} and 8.0e1:
+    pass
+while 0.1 and 0x1 + {} and 8.0e1:
+    pass
+while 0.1 and 0x0 + {} and 8.0e1:
+    pass
+while {}.B.H𝟊 + 0.1 and 0x1 and {}:
+    pass
+while 0.1 and 0o1 @ {} and 8.0e3:
+    pass
+while 0.1 and 0o_7 @ {} and 8.0:
+    pass
+while 0.1 and 0o_7 @ {} and 8.0e3:
+    pass
+while 0.8 and 0x_1 @ {} and 0.1e-1:
+    pass
+while {}.A.A.A0:  # type: ignore
+    pass
+
+
+class Ƒ:
+    pass
+
+
+class ʊ:  #
+    pass
+
+
+class A:
+    pass
+
+
+while {}.ʒ3.B.B.A.A & ... and 0x1 and ...:
+    pass
+while ... and ...:
+    pass
